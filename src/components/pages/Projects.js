f00b9f5f861aec4ce176ba5622c631268361fe56
Sarah~ <s.k.miller422@gmail.com>
2023-05-17 10:56:40 -0400

    git hub

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Footer from "../footer/index.js";
+import photo1 from "../../image/animalfan.png"
+import photo2 from "../../image/pizzasite.png"
+import photo3 from "../../image/abedsderm.png"
 
 export default function Projects() {
   return (
@@ -14,7 +17,7 @@ export default function Projects() {
             <a href="https://github.com/Jsuttle2/P1-animal-fan-tool"  target="_blank" rel="noopener noreferrer"
               className="text-purple-500 hover:text-blue-300 underline"
               > <img
-              src="image/animalfan.png"
+              src={photo1}
               alt="animalfan"
               className="w-40 h-40 mr-2 inline-block"
             />
@@ -24,7 +27,7 @@ export default function Projects() {
             <a href="https://github.com/TiffanyParks/HTM-Pizza-More"  target="_blank" rel="noopener noreferrer"
               className="text-pink-500 hover:text-blue-600 underline"
               > <img
-              src="image/pizzasite.png"
+              src={photo2}
               alt="pizzasite"
               className="w-40 h-40 mr-2 inline-block"
             />
@@ -34,7 +37,7 @@ export default function Projects() {
             <a href="https://github.com/IsaiahGz/project-3"  target="_blank" rel="noopener noreferrer"
               className="text-purple-700 hover:text-blue-900 underline"
               > <img
-              src="image/abedsderm.png"
+              src={photo3}
               alt="abedsderm"
               className="w-40 h-40 mr-2 inline-block"
             />
